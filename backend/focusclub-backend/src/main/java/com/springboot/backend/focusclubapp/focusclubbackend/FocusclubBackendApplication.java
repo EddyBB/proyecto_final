03f08c420bdafc661d176e5ff7e6df1b84af0f43
EddyBB <eddy.gonzalez.chavez.al@iespoligonosur.org>
2024-05-20 12:39:08 +0200

    primer commit

---
+++ backend/focusclub-backend/src/main/java/com/springboot/backend/focusclubapp/focusclubbackend/FocusclubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.backend.focusclubapp.focusclubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusclubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusclubBackendApplication.class, args);
+	}
+
+}
---
+++ backend/focusclub-backend/src/test/java/com/springboot/backend/focusclubapp/focusclubbackend/FocusclubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.backend.focusclubapp.focusclubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusclubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
